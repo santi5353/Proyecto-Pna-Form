530b4ea77e05b4854f533deedd86742cf485dafc
santigo solano <santiagosolano036@gmail.com>
2022-07-21 16:24:52 -0300

    fragmento de paises

--- js/animacionesForm.js
+++ js/animacionesForm.js
@@ -11,3 +11,27 @@ function ocultarR(){
 
     
 }
+
+//Creacion de Fragmento para los select pais
+
+
+const paises = [
+    {code:"Ar", name:"Argentina"},
+    {code:"Bra", name:"Brazil"},
+    {code:"Pa", name:"Paraguay"},
+    {code:"Uru", name:"Uruguay"},
+    {code:"Bo", name:"Bolivia"},
+    {code:"Ve", name:"Venezuela"},
+]
+
+const selectPais = document.getElementById("nacionalidad");
+const frag = document.createDocumentFragment();
+
+paises.forEach(function (pais) {
+    const option = document.createElement("option");
+    option.value = pais.code;
+    option.textContent = pais.name;
+    frag.appendChild(option);
+});
+
+selectPais.appendChild(frag);
