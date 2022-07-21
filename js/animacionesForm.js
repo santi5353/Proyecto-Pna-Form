a8327c08e1485e25069e874e7681cbe6c192556c
santigo solano <santiagosolano036@gmail.com>
2022-07-21 15:02:05 -0300

    script ocultar o mostrar "Recuperar contraseña"

---
+++ js/animacionesForm.js
@@ -0,0 +1,13 @@
+
+
+//OCULTAR O MOSTRAR RECUPERACION DE CONTRASEÑA---
+function mostrarR(){
+    document.getElementById('recuperarForm').style.display ='block';
+    document.getElementById('loginForm').style.display='none';
+}
+function ocultarR(){
+    document.getElementById('loginForm').style.display ='block';
+    document.getElementById('recuperarForm').style.display ='none';
+
+    
+}
