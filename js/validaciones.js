53c6c13195eaafa6bcea1a1784f7c6a712debffe
santigo solano <santiagosolano036@gmail.com>
2022-08-22 12:54:37 -0300

    validaciones

--- js/validaciones.js
+++ js/validaciones.js
@@ -2,9 +2,11 @@ const formulario = document.getElementById('registerForm');
 const inputs = document.querySelectorAll('#registerForm input');
 
 const expresiones = {
+   
+   dni: /^\d{8,}$/, // 7 a 14 numeros.
 	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	password: /^.{4,12}$/, // 4 a 12 digitos.
+	//assword: /^.{4,12}$/, // 4 a 12 digitos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
 };
@@ -12,12 +14,22 @@ const expresiones = {
 const validarFormulario = (e) => {
  switch  (e.target.name){
     case  "dni":
+      if (expresiones.dni.test()){
+
+      }
    
     break;
     case  "numTramite":
+      if (expresiones.dni.test()){
+
+      }
    
     break;
     case  "ejemplar":
+      if (expresiones.dni.test()){
+
+      }
+      
    
     break;
     case  "apellido":
